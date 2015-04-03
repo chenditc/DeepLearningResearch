7132b4621ad11d64efe29449f5c76c2ef2e52f4d
chenditc <chenditc@umich.edu>
2015-04-03 19:02:20 +0000

    Start train model workflow

---
+++ system/Workflow/TrainModel.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# This module include the process of training a deep learning model
+
+class TrainModel:
+
+    def __init__(self, data_id):
+        # private variables:
+        self._data_id = data_id
+
+    def startTraining():
+        # Use Dataloader class to load data set.
+
+
+
+
+        # Get required meta data from data set, eg. dimensionality
+        # Create training model
+        # Call trainModel(trainingSet, validationSet) to train the model
+        # Call testModel(testSet) to test the model
