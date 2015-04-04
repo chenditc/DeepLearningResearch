cd13bde6b5d9990f3bada777d7c13b6fc1df9715
chenditc <chenditc@umich.edu>
2015-04-04 14:56:11 +0000

    refactor LogisticRegression.py to fit in TrainModel pipeline

--- system/Workflow/TrainModel.py
+++ system/Workflow/TrainModel.py
@@ -2,19 +2,130 @@
 
 # This module include the process of training a deep learning model
 
+import numpy
+import theano
+import theano.tensor as T
+import LogisticRegression
+
 class TrainModel:
 
     def __init__(self, data_id):
         # private variables:
         self._data_id = data_id
 
-    def startTraining():
+
+
+    ##
+    # @brief 
+    #    Function that loads the dataset into shared variables
+    #
+    #    The reason we store our dataset in shared variables is to allow
+    #    Theano to copy it into the GPU memory (when code is run on GPU).
+    #    Since copying data into the GPU is slow, copying a minibatch everytime
+    #    is needed (the default behaviour if the data is not in a shared
+    #    variable) would lead to a large decrease in performance.
+    #    
+    # @param inputData      the input data to model 
+    # @param outputData     the output data to model
+    # @param borrow         if enable shallow copy or not
+    # @param isClassifier   if this is a classifier
+    #
+    # @return 
+    @staticmethod
+    def shared_dataset(inputData, outputData, borrow=True, isClassifier = True):
+        data_x = inputData
+        data_y = outputData
+
+        # if the data is for a classifier, use the first column of y only
+        # and also change it to an array of scalar
+        if isClassifier:
+            data_y = data_y[:,0]
+
+        # Create share variable from numpy array
+        shared_x = theano.shared(numpy.asarray(data_x,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(numpy.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        # When storing data on the GPU it has to be stored as floats
+        # therefore we will store the labels as ``floatX`` as well
+        # (``shared_y`` does exactly that). But during our computations
+        # we need them as ints (we use labels as index, and if they are
+        # floats it doesn't make sense) therefore instead of returning
+        # ``shared_y`` we will have to cast it to int. This little hack
+        # lets ous get around this issue
+        return shared_x, T.cast(shared_y, 'int32')
+
+    def load_data(self, dataset):
+        ''' Loads the dataset
+
+        :type dataset: string
+        :param dataset: the path to the dataset (here MNIST)
+        '''
+
+        #############
+        # LOAD DATA #
+        #############
+
+        #train_set, valid_set, test_set format: tuple(input, target)
+        #input is an numpy.ndarray of 2 dimensions (a matrix)
+        #witch row's correspond to an example. target is a
+        #numpy.ndarray of 1 dimensions (vector)) that have the same length as
+        #the number of rows in the input. It should give the target
+        #target to the example with the same index in the input.
+        import DataLoader
+        dataLoader = DataLoader.DataLoader()
+        dataLoader.downloadData("mnist_data_set")
+        trainingInput, trainingOutput = dataLoader.getTrainingSet() 
+        validationInput, validationOutput = dataLoader.getValidationSet() 
+        testInput, testOutput = dataLoader.getTestSet() 
+
+
+        # Create 
+        train_set_x, train_set_y = TrainModel.shared_dataset(trainingInput, trainingOutput)
+        valid_set_x, valid_set_y = TrainModel.shared_dataset(validationInput, validationOutput)
+        test_set_x, test_set_y = TrainModel.shared_dataset(testInput, testOutput)
+
+        rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
+                (test_set_x, test_set_y)]
+        return rval
+
+
+
+    def startTraining(self, isClassifier = True):
         # Use Dataloader class to load data set.
+        datasets = self.load_data(self._data_id)
 
+        train_set_x, train_set_y = datasets[0]
+        valid_set_x, valid_set_y = datasets[1]
+        test_set_x, test_set_y = datasets[2]
 
-
+        xValue = train_set_x.get_value()
+        yValue = train_set_y.eval()
 
         # Get required meta data from data set, eg. dimensionality
+        trainSize = len(xValue)
+        inputDim = len(xValue[0])
+        outputDim = 0
+        if (isClassifier):
+            outputDim = max(yValue) + 1
+        else:
+            outputDim = len(yValue[0])
+
+        validSize = len(valid_set_x.get_value())
+        testSize = len(test_set_x.get_value())
+
         # Create training model
+        classifier = LogisticRegression.LogisticRegression(n_in = inputDim, n_out = outputDim)
+
+
         # Call trainModel(trainingSet, validationSet) to train the model
+        classifier.trainModel(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_set_y)
+
+
         # Call testModel(testSet) to test the model
+
+if __name__ == "__main__" :
+    trainer = TrainModel(data_id = 'mnist_data_set')
+    trainer.startTraining()
