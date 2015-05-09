777808980c694641208ef50377e4a360d92970a6
chenditc <chenditc@umich.edu>
2015-05-09 21:10:31 +0000

    Add dynamic load model in training workflow

--- system/Workflow/TrainModel.py
+++ system/Workflow/TrainModel.py
@@ -18,9 +18,10 @@ import DataLoader
 
 class TrainModel:
 
-    def __init__(self, data_id):
+    def __init__(self, data_id, model_id):
         # private variables:
         self._data_id = data_id
+        self.model_id = model_id
 
 
     def startTraining(self, isClassifier = True):
@@ -40,8 +41,14 @@ class TrainModel:
 #        classifier = LogisticRegression.LogisticRegression(n_in = inputDim, n_out = outputDim)
 #        print "Initializing model: ", MultilayerPerceptron.MultilayerPerceptron.__name__
 #        classifier = MultilayerPerceptron.MultilayerPerceptron(n_in = inputDim, n_out = outputDim)
-        print "Initializing model: ", RecurrentNN.RecurrentNN.__name__
-        classifier = RecurrentNN.RecurrentNN(n_in = inputDim, n_out = outputDim)
+#        print "Initializing model: ", RecurrentNN.RecurrentNN.__name__
+#        classifier = RecurrentNN.RecurrentNN(n_in = inputDim, n_out = outputDim)
+
+        print "Initializing model: ", self.model_id
+        classifierModule = __import__(self.model_id)
+        classifierClass = getattr(classifierModule, self.model_id)
+        classifier = classifierClass(n_in = inputDim, n_out = outputDim)
+
 
         
         # Initialize trainer, here we use Early stopping 
@@ -60,12 +67,14 @@ if __name__ == "__main__" :
 
     parser = argparse.ArgumentParser(description='Training Entrance.')
     parser.add_argument('-d', '--data_id', dest='data_id', help='the data used to train')
+    parser.add_argument('-m', '--model_id', dest='model_id', help='the model used to train')
+
     args = parser.parse_args()
 
-    if (args.data_id == None):
+    if (args.data_id == None or args.model_id == None ):
         parser.print_help()
         quit()
 
 
-    trainer = TrainModel(data_id = args.data_id)
+    trainer = TrainModel(data_id = args.data_id, model_id = args.model_id)
     trainer.startTraining()
