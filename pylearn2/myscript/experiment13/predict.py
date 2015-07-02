1f3d1c8c95a6b7049e85b62aedaf7a1743cd1e38
chenditc <chenditc@umich.edu>
2015-07-02 13:17:43 +0000

    add experiment13

---
+++ pylearn2/myscript/experiment13/predict.py
@@ -0,0 +1,50 @@
+
+from pylearn2.utils import serial
+import load_security
+import theano
+
+dataset = load_security.load_security('all', startDate = '2014-03-01', stopDate = '2014-05-01', days = 5, threshold = 0)
+#dataset = load_shcomp_index.load_security('/home/ubuntu/test.txt', start = 0, stop=130, days = 5)
+
+x_test = dataset.X
+
+model_path = './mlp_regression1.pkl'
+model = serial.load( model_path )
+X = model.get_input_space().make_theano_batch()
+Y = model.fprop( X )
+
+f = theano.function( [X], Y ,allow_input_downcast=True)
+
+y = f( x_test )
+
+right = 0
+wrong = 0
+myMoney = 100.0
+standard = 100.0
+for i in range(len(y)):
+    signCorrect = False 
+    up = True
+    if dataset.y[i][0] < 0:
+        up = False
+    
+    before = myMoney
+    threshold = 0
+    if y[i][0] > threshold:
+        myMoney += myMoney * dataset.y[i][0] / 100.0
+    elif y[i][0] < -threshold:
+        myMoney -= myMoney * dataset.y[i][0] / 100.0
+
+    standard += standard * dataset.y[i][0] / 100.0
+
+    if (myMoney - before) / before > 0:
+        signCorrect = True
+        right += 1
+    elif (myMoney - before) / before < 0:
+        wrong += 1
+    else:
+        continue
+
+    print y[i][0], dataset.y[i][0], up, signCorrect
+
+print "Right: ", right, " Wrong: ", wrong
+print "MyMoney: ", myMoney, " standard: ", standard 
